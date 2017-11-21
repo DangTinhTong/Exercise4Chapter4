318cb0cb45ce26d0f0674bf27630fa6b734cdde2
admin <dangtinhtong@gmail.com>
2017-11-21 14:16:40 +0700

    First commit

--- Exercise4/main.m
+++ Exercise4/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  Exercise4
 //
-//  Created by admin on 11/21/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/21/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -11,7 +11,10 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSlog(@"Hello, World!");
+        float x=2.55;
+        float y=3*x*x*x-5*x*x+6;
+        NSLog(@"3x^3-5x^2+6 = %f",y);
     }
     return 0;
 }
